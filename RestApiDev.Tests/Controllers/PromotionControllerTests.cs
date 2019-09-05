ba552ac6358f478389c91b1f3b3b07692b0e05ec
Prasanna Kumar <dommalapati.chowdary@gmail.com>
2019-09-05 01:18:20 +0100

    fix test

--- RestApiDev.Tests/Controllers/PromotionControllerTests.cs
+++ RestApiDev.Tests/Controllers/PromotionControllerTests.cs
@@ -1,5 +1,8 @@
+using AutoMapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using RestApiDev.API.Controllers;
+using RestApiDev.Library.Data;
 using System;
 
 namespace RestApiDev.API.Tests
@@ -7,6 +10,16 @@ namespace RestApiDev.API.Tests
     [TestClass]
     public class PromotionControllerTests
     {
+        private IDataContext DataContext;
+        private IMapper Mapper;
+
+        [TestInitialize]
+        public void Init()
+        {
+            DataContext = new Mock<IDataContext>().Object;
+            Mapper = new Mock<IMapper>().Object;
+        }
+
         [TestMethod]
         public void CreatePromotion_Returns_Guid()
         {
@@ -17,7 +30,7 @@ namespace RestApiDev.API.Tests
         [ExpectedException(typeof(ArgumentNullException))]
         public void Ctor_Throws_Null_Story()
         {
-            _ = new PromotionController(null);
+            _ = new PromotionController(DataContext,Mapper);
         }
     }
 }
